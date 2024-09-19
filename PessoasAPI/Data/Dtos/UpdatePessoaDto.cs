ecb2c2dc9f1f1a6eff5f4b628ec15715eca54bc0
higor <higor@DESKTOP-FLEV9EM>
2024-09-19 20:15:24 -0300

    Adicionar arquivos de projeto.

---
+++ PessoasAPI/Data/Dtos/UpdatePessoaDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PessoasAPI.Data.Dtos;
+
+public class UpdatePessoaDto
+{
+    [Required(ErrorMessage = "O Nome é Obrogatório")]
+    [StringLength(100, ErrorMessage = "O nome não pode ultrapassar 100 caracteres")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O CPF é Obrogatório")]
+    [StringLength(11, ErrorMessage = "O CPF não pode ultrapassar 11 caracteres")]
+    public string CPF { get; set; }
+
+    [Required(ErrorMessage = "A Data de Nascimento é Obrogatória")]
+    public string Nascimento { get; set; }
+
+    [Required(ErrorMessage = "O Status Ativo ou Inativo é Obrogatório")]
+
+    public string Status { get; set; }
+}
